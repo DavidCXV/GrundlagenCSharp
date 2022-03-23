5153cd2f8001b0b676f947816c5f562609a4dcd9
David S <david.s58@gso.schule.koeln>
2022-03-23 12:48:05 +0100

    Projekt fertig gestellt

---
+++ Methoden_01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Methoden_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int amount = 0;
+            amount = outputoftextTimesX();
+
+            Console.WriteLine("\nAnzahl der Buchstaben: {0}", amount);
+
+        }
+
+        static int outputoftextTimesX() 
+        {
+
+            Console.WriteLine("Bitte geben sie ein beliebiges Wort ein");
+            string word = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Bestimmen sie nun die Anzahl der ausgegebenen Wörtern");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            for(int i=0; i < x; i++) 
+            {
+
+                Console.WriteLine(word);
+                            
+            }
+
+            int letters = word.Length * x;
+
+            return letters;
+            
+        }
+        
+    
+    }
+}
