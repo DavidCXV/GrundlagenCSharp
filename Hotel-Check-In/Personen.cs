fc0cb06c7dbe2b0b8f8e4cb454c73e4ed4f0e8e7
David S <david.s58@gso.schule.koeln>
2022-03-30 12:55:17 +0200

    Projekt: Hotel Checkin angefangen

---
+++ Hotel-Check-In/Personen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Check_In
+{
+    class Personen
+    {
+        #region Konstruktoren 
+        public Personen() : this(0," "," ", 0, 0) { }
+        public Personen(short rn):this(rn, " ", " ", 0, 0) { }
+        public Personen(short rn, string fn):this(rn, fn, " ", 0, 0) { }
+        public Personen(short rn, string fn, string sn):this(rn, fn, sn, 0, 0) { }
+        public Personen(short rn, string fn, string sn, short sl):this(rn, fn, sn, sl, 0) { }
+        public Personen(short rn, string fn, string sn, short sl, double am)
+        {
+            roomnumber = rn;
+            firstname = fn;
+            surname = sn;
+            sleepovers = sl;
+            invoiceamount = am; 
+        }
+        #endregion
+
+        #region Eigenschaften der Instanz
+        public short roomnumber { get; set; }
+        public short sleepovers { get; set; }
+        public double invoiceamount { get; set; }
+        public string firstname { get; set; }
+        public string surname { get; set; }
+        #endregion
+
+
+
+    }
+}
--- Hotel-Check-In/Program.cs
+++ Hotel-Check-In/Program.cs
@@ -6,7 +6,39 @@ namespace Hotel_Check_In
     {
         static void Main(string[] args)
         {
-            
+            Personen gast1 = new Personen();
+
+            gast1.roomnumber = 45;
+            gast1.firstname = "John";
+            gast1.surname = "Lennon";
+            gast1.sleepovers = 1;
+            gast1.invoiceamount = 82.46;
+
+            Personen gast2 = new Personen();
+
+            gast2.roomnumber = 45;
+            gast2.firstname = "Paul";
+            gast2.surname = "McCartney";
+            gast2.sleepovers = 4;
+            gast2.invoiceamount = 329.84;
+
+            Personen gast3 = new Personen();
+
+            gast3.roomnumber = 45;
+            gast3.firstname = "George";
+            gast3.surname = "Harrison";
+            gast3.sleepovers = 12;
+            gast3.invoiceamount = 989.52;
+
+            Personen gast4 = new Personen();
+
+            gast4.roomnumber = 45;
+            gast4.firstname = "Ringo";
+            gast4.surname = "Starr";
+            gast4.sleepovers = 3;
+            gast4.invoiceamount = 247.38;
+
+            //Personen[] gaeste = 
         }
     }
 }
